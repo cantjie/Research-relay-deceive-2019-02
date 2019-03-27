9d755fecda153123696690be23a13c0bb0ad0188
Cantjie <Cantjie@163.com>
2019-03-27 16:06:57 +0800

    init commit

---
+++ optimize_rotation_angle_map.m
@@ -0,0 +1,8 @@
+% optimize_rotation_angle.m
+% purpose: to optimize the mapping from bits(symbol)to rotation angle.
+% To make the adjacent symbols(hamming distance = 1) have the least
+% difference.
+%
+% version 1, created at 2019/03/27, author:Cantjie(cantjie@163.com)
+
+clc,clear
