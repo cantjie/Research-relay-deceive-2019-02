033d6f9dab1941c218794f3e906a97cb4506ca4b
Cantjie <Cantjie@163.com>
2019-05-18 10:07:26 +0800

    reorganize

--- GeneticAlgorithm/GeneticAlgorithm/main.cpp
+++ GeneticAlgorithm/GeneticAlgorithm/main.cpp
@@ -4,13 +4,13 @@
 #include <cmath>
 
 #define DNA_LENGTH 16
-#define POPULATION 20000
-#define N_GENERATION 300
-#define CROSSOVER_RATE_PERCENT 20
-#define MUTATE_RATE_PERMILLAGE 20 
+#define POPULATION 200000
+#define N_GENERATION 40
+//#define CROSSOVER_RATE_PERCENT 20
+#define MUTATE_RATE_PERMILLAGE 200  
 
-short G_perms_A[POPULATION][DNA_LENGTH] = {0};
-short G_perms_B[POPULATION][DNA_LENGTH] = {0};
+short G_perms_A[POPULATION][DNA_LENGTH] = { 0 };
+short G_perms_B[POPULATION][DNA_LENGTH] = { 0 };
 short *G_parents_ptr[POPULATION];
 short *G_children_ptr[POPULATION];
 double G_children_fitness[POPULATION] = { 0 };
@@ -36,12 +36,12 @@ short G_adjacent_array[DNA_LENGTH][2] = {
 };
 
 double Pe1[DNA_LENGTH] = { 0.0564953,0.195519,0.5,0.804481,0.943505,0.98081,0.987499,0.98081,0.943505,0.804481,0.5,0.195519,0.0564953,0.0191905,0.0125008,0.0191905 };
-	//at SNR = 4dB.
+//at SNR = 4dB.
 double Pe2[DNA_LENGTH] = { 0.0564953,0.0191905,0.0125008,0.0191905,0.0564953,0.195519,0.5,0.804481,0.943505,0.98081,0.987499,0.98081,0.943505,0.804481,0.5,0.195519 };
 
-short G_adjacent_difference[DNA_LENGTH][2] = {0};
+short G_adjacent_difference[DNA_LENGTH][2] = { 0 };
 
-void calculateDifference(short *vector) 
+void calculateDifference(short *vector)
 {
 	static short i = 0;
 	static short j = 0;
@@ -53,11 +53,27 @@ void calculateDifference(short *vector)
 	}
 }
 
-void initPerms(short *vectors[]) 
+void initPerms(short *vectors[])
 {
 	short base_perm[DNA_LENGTH] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
 	int i = 0, j = 0, k = 0;
 	short temp = 0;
+
+	base_perm[0] = 0;
+	for (i = 1; i < DNA_LENGTH; i++) {
+		while (temp = rand() % (DNA_LENGTH - 1) + 1) {
+			for (j = 0; j < i; j++) {
+				if (temp == base_perm[j]) {
+					break;
+				}
+			}
+			if (j == i) {
+				break;
+			}
+		}
+		base_perm[i] = temp;
+	}
+
 	for (i = 0; i < POPULATION; i++) {
 		memcpy(vectors[i], base_perm, DNA_LENGTH * sizeof(short));
 		j = rand() % (DNA_LENGTH - 1) + 1; // to fix the first location
@@ -68,7 +84,7 @@ void initPerms(short *vectors[])
 	}
 }
 
-void calculateFitness(short *vector[],double *fitness) 
+void calculateFitness(short *vector[], double *fitness)
 {
 	static int idx = 0; // idx of perms
 	static int i = 0, j = 0, k = 0;
@@ -89,10 +105,10 @@ void calculateFitness(short *vector[],double *fitness)
 	}
 }
 
-void crossover(short *parents_vector[], short *children_vector[]) 
+void crossover(short *parents_vector[], short *children_vector[])
 {
 	int father_idx = 0;
-	int mother1_idx,mother2_idx = 0;
+	int mother1_idx, mother2_idx = 0;
 	int child_idx = 0;
 	short i = 0, j = 0;
 	short *father = NULL, mother1[DNA_LENGTH], mother2[DNA_LENGTH], *child = NULL;
@@ -107,37 +123,38 @@ void crossover(short *parents_vector[], short *children_vector[])
 		father = parents_vector[father_idx];
 		mother1_idx = POPULATION / 2 + rand() % (POPULATION / 2);
 		mother2_idx = POPULATION / 2 + rand() % (POPULATION / 2);
-		while (mother1_idx == mother2_idx){
+		//mother1_idx = rand() % POPULATION;
+		while (mother1_idx == mother2_idx) {
 			mother2_idx = POPULATION / 2 + rand() % (POPULATION / 2);
 		}
 		child = children_vector[child_idx];
-		
+
 		//// with mother1, father retain 0 1 6 7 8 9 14 15
 		//if (rand() % 100 > CROSSOVER_RATE_PERCENT) {
 		//	// there is some probability that they do not mate;
 		//	memcpy(child, father, DNA_LENGTH * sizeof(short));
 		//}
 		//else {
-			memcpy(mother1, parents_vector[mother1_idx], DNA_LENGTH * sizeof(short));
-			memcpy(child, father, DNA_LENGTH * sizeof(short));
-			for (i = 0; i < DNA_LENGTH / 2; i++) {
-				for (j = 0; j < DNA_LENGTH; j++) {
-					if (mother1[j] == father[father_retain_A[i]]) {
-						mother1[j] = DNA_LENGTH;
-						break;
-					}
+		memcpy(mother1, parents_vector[mother1_idx], DNA_LENGTH * sizeof(short));
+		memcpy(child, father, DNA_LENGTH * sizeof(short));
+		for (i = 0; i < DNA_LENGTH / 2; i++) {
+			for (j = 0; j < DNA_LENGTH; j++) {
+				if (mother1[j] == father[father_retain_A[i]]) {
+					mother1[j] = DNA_LENGTH;
+					break;
 				}
 			}
-			j = 0;
-			for (i = 0; i < DNA_LENGTH / 2; i++) {
-				for (; j < DNA_LENGTH; j++) {
-					if (DNA_LENGTH != mother1[j]) {
-						child[father_abandon_A[i]] = mother1[j];
-						mother1[j] = DNA_LENGTH;
-						break;
-					}
+		}
+		j = 0;
+		for (i = 0; i < DNA_LENGTH / 2; i++) {
+			for (; j < DNA_LENGTH; j++) {
+				if (DNA_LENGTH != mother1[j]) {
+					child[father_abandon_A[i]] = mother1[j];
+					mother1[j] = DNA_LENGTH;
+					break;
 				}
 			}
+		}
 		//}
 		child_idx++;
 		child = children_vector[child_idx];
@@ -149,39 +166,38 @@ void crossover(short *parents_vector[], short *children_vector[])
 		//	memcpy(child, mother2, DNA_LENGTH * sizeof(short));
 		//}
 		//else {
-			memcpy(mother2, parents_vector[mother2_idx], DNA_LENGTH * sizeof(short));
-			memcpy(child, father, DNA_LENGTH * sizeof(short));
-			for (i = 0; i < DNA_LENGTH / 2; i++) {
-				for (j = 0; j < DNA_LENGTH; j++) {
-					if (mother2[j] == father[father_retain_B[i]]) {
-						mother2[j] = DNA_LENGTH;
-						break;
-					}
+		memcpy(mother2, parents_vector[mother2_idx], DNA_LENGTH * sizeof(short));
+		memcpy(child, father, DNA_LENGTH * sizeof(short));
+		for (i = 0; i < DNA_LENGTH / 2; i++) {
+			for (j = 0; j < DNA_LENGTH; j++) {
+				if (mother2[j] == father[father_retain_B[i]]) {
+					mother2[j] = DNA_LENGTH;
+					break;
 				}
 			}
-			j = 0;
-			for (i = 0; i < DNA_LENGTH / 2; i++) {
-				for (; j < DNA_LENGTH; j++) {
-					if (DNA_LENGTH != mother2[j]) {
-						child[father_abandon_B[i]] = mother2[j];
-						mother2[j] = DNA_LENGTH;
-						break;
-					}
+		}
+		j = 0;
+		for (i = 0; i < DNA_LENGTH / 2; i++) {
+			for (; j < DNA_LENGTH; j++) {
+				if (DNA_LENGTH != mother2[j]) {
+					child[father_abandon_B[i]] = mother2[j];
+					mother2[j] = DNA_LENGTH;
+					break;
 				}
 			}
+		}
 		//}
 		child_idx++;
-		//child = children_vector[child_idx];
 	}
-	
+
 }
 
-void mutate(short *vector[]) 
+void mutate(short *vector[])
 {
 	int i = 0;
 	int k = 0, j = 0;
 	short temp;
-	
+
 	for (i = 0; i < POPULATION; i++) {
 		if (rand() % 1000 < MUTATE_RATE_PERMILLAGE) {
 			k = rand() % (DNA_LENGTH - 1) + 1;
@@ -202,8 +218,8 @@ void select()
 
 	memcpy(child_copy, G_children_ptr[0], sizeof(short) * DNA_LENGTH * POPULATION);
 	for (i = 0; i < POPULATION; i++) {
-		G_parents_fitness[i] = exp(G_parents_fitness[i]+20);
-		G_children_fitness[i] = exp(G_children_fitness[i]+20);
+		G_parents_fitness[i] = exp(G_parents_fitness[i] + 20);
+		G_children_fitness[i] = exp(G_children_fitness[i] + 20);
 		sum += G_parents_fitness[i];
 		sum += G_children_fitness[i];
 	}
@@ -211,20 +227,30 @@ void select()
 		G_parents_fitness[i] = G_parents_fitness[i] / sum * POPULATION;
 		G_children_fitness[i] = G_children_fitness[i] / sum * POPULATION;
 	}
-	
+	//for (i = 0; i < POPULATION; i++) {
+	//	G_parents_fitness[i] = G_parents_fitness[i] / sum;
+	//	G_children_fitness[i] = G_children_fitness[i] / sum;
+	//}
+
 	i = 0;
-	for (j = 0; j < POPULATION; j++) {
-		if (rand() / (double)(RAND_MAX) < G_parents_fitness[j]) {
-			memcpy(G_children_ptr[i], G_parents_ptr[j], sizeof(short)*DNA_LENGTH);
-			i++;
+	//for (i = 0; i < POPULATION;) {
+	while (POPULATION > i) {
+		for (j = 0; j < POPULATION; j++) {
+			if (rand() / (double)(RAND_MAX) < G_parents_fitness[j]) {
+				memcpy(G_children_ptr[i], G_parents_ptr[j], sizeof(short)*DNA_LENGTH);
+				G_parents_fitness[j] -= 1;
+				i++;
+			}
+			if (i == POPULATION) { break; }
+			if (rand() / (double)(RAND_MAX) < G_children_fitness[j]) {
+				memcpy(G_children_ptr[i], child_copy[j], sizeof(short)*DNA_LENGTH);
+				G_children_fitness[j] -= 1;
+				i++;
+			}
+			if (i == POPULATION) { break; }
 		}
-		if (i == POPULATION) { break; }
-		if (rand() / (double)(RAND_MAX) < G_children_fitness[j]) {
-			memcpy(G_children_ptr[i], child_copy[j], sizeof(short)*DNA_LENGTH);
-			i++;
-		}
-		if (i == POPULATION) { break; }
 	}
+
 
 	for (i = 0; i < POPULATION; i++) {
 		temp = G_parents_ptr[i];
@@ -233,11 +259,21 @@ void select()
 	}
 }
 
+int savePerm(FILE *fp, short *perm) {
+	if (NULL == fp) {
+		printf("Error! Failed to save opt perms");
+		return 1;
+	}
+	fprintf(fp, "%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd\n", perm[0], perm[1], perm[2], perm[3], perm[4], perm[5], perm[6], perm[7], perm[8], perm[9], perm[10], perm[11], perm[12], perm[13], perm[14], perm[15]);
+	return 0;
+}
+
 int main() {
 	int i = 0, j = 0;
 	int generation = 0;
 	double max_fitness = 0;
 	int max_fitness_idx = 0;
+	FILE *fp = NULL;
 
 	time_t t;
 
@@ -250,13 +286,13 @@ int main() {
 
 	initPerms(G_parents_ptr);
 	for (generation = 0; generation < N_GENERATION; generation++) {
-		crossover(G_parents_ptr,G_children_ptr);
-		calculateFitness(G_children_ptr,G_children_fitness);
-		calculateFitness(G_parents_ptr,G_parents_fitness);
+		crossover(G_parents_ptr, G_children_ptr);
+		calculateFitness(G_children_ptr, G_children_fitness);
+		calculateFitness(G_parents_ptr, G_parents_fitness);
 		mutate(G_children_ptr);
 		select(); // new parents generated.
 
-		printf("G:%d\n",generation+1);
+		printf("G:%d\n", generation + 1);
 		//find max
 		calculateFitness(G_children_ptr, G_children_fitness);
 		max_fitness_idx = 0;
@@ -273,8 +309,19 @@ int main() {
 		}
 		printf("\n");
 	}
-	
 
+	fp = fopen("fitness.csv", "w");
+	fprintf(fp, "%.6lf\n", max_fitness);
+	fclose(fp);
+
+	fp = fopen("perms.csv", "w");
+	for (i = 0; i < POPULATION; i++)
+	{
+		if (abs(G_children_fitness[i] - max_fitness)<1e-5) {
+			savePerm(fp, G_children_ptr[i]);
+		}
+	}
+	fclose(fp);
 
 	system("pause");
 	return 0;
---
+++ optimize_mapping/calculate_Q_table.m
@@ -0,0 +1,22 @@
+clc,clear
+
+format long;
+N = 16;
+SNR_db = 4;
+
+theta = 0 : 2*pi/N : 2*pi - 1e-5;
+a = cos(pi/4 + theta);
+b = sin(pi/4 + theta);
+
+SNR = 10^(SNR_db/10);
+pre_factor = sqrt(2*SNR);
+
+x1 = pre_factor * a;
+x2 = pre_factor * b;
+
+Pe1 = qfunc(x1);
+Pe2 = qfunc(x2);
+
+index = 0 : N-1;
+filename = ['Pe_at_SNR_',num2str(SNR_db),'.csv'];
+dlmwrite(filename,[Pe1;Pe2],'delimiter',',','precision',6);
